8788a2498f6405385decced84196064a9a21fe1a
Alicia X <aliciaxu@yahoo.com>
2017-04-09 00:47:47 -0700

    video screenshot!

--- maingame/photo.js
+++ maingame/photo.js
@@ -5,6 +5,7 @@ navigator.getUserMedia = (navigator.getUserMedia ||
 
 var video;
 var webcamStream;
+var canvas, ctx;
 
 function startWebcam() {
     if (navigator.getUserMedia) {
@@ -36,5 +37,29 @@ function startWebcam() {
 
 function init() {
     startWebcam();
+    setTimeout(function () {
+        alert("YO");
+        getImages();
+    }, 5000);
+
+}
+
+function getImages() {
+
+    for (i = 0; i < 50; i++) {
+        console.log(i);
+
+        setTimeout(function () {
+            canvas = document.getElementById("myCanvas");
+            ctx = canvas.getContext('2d');
+            snapshot();
+        }, 10000);
+    }
+}
+
+function snapshot() {
+    // Draws current image from the video element into the canvas
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    alert("NEW PHOTO");
 }
 window.onload = init();
