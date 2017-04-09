d6940e90d96c6972a548deb93e3074c168f58379
Alicia X <aliciaxu@yahoo.com>
2017-04-09 06:24:14 -0700

    game added into html

--- gameinjavascripthtml/script.js
+++ gameinjavascripthtml/script.js
@@ -1,5 +1,3 @@
-
-
 var myGamePiece;
 var myObstacles = [];
 var myPlatform;
@@ -13,71 +11,71 @@ var startY = 310;
 var startHeight = 60;
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    canvas: document.createElement("canvas"),
+    start: function () {
         this.canvas.width = 600;
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//        document.getElementById("game").appendChild(canvas);
+                document.body.insertBefore(this.canvas, document.body.childNodes[1]);
+//                var c = document.body.childNodes;
+//                console.log(c)
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, startInterval);
     },
-    clear : function(){
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    stop : function(){
+    stop: function () {
         clearInterval(this.interval);
     }
 }
 
 
 function startGame() {
-    myGamePiece = new component (30, 60, "red", 10, 310);
+    myGamePiece = new component(30, 60, "red", 10, 310);
     myObstacle = new component(10, 200, "green", 300, 100);
-    myPlatform = new component(600, 25, "yellow", 0,  375);
+    myPlatform = new component(600, 25, "yellow", 0, 375);
     startInterval = 10;
     intervalCount = 0; // after every 5 intervalCount, decrease interval
     characterMinY = 310;
     characterMaxY = 350;
     myGameArea.start();
-    
-    
+
+
 }
 
-function component(width, height, color, x, y){
+function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function(){ //calling update recreates game piece
+    this.update = function () { //calling update recreates game piece
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function(){
-        if (ducking && this.height == startHeight){
+    this.newPos = function () {
+        if (ducking && this.height == startHeight) {
             this.height -= duckHeight;
             this.y += duckHeight;
-    
-        }
-        else if (!ducking && this.height != startHeight && !jumping){
+
+        } else if (!ducking && this.height != startHeight && !jumping) {
             this.height += duckHeight;
             this.y -= duckHeight;
-            
-        }
-        else if(jumping && this.y == startY){
+
+        } else if (jumping && this.y == startY) {
             this.y -= jumpHeight;
-        }
-        else if (!jumping && this.y != startY && !ducking){
+        } else if (!jumping && this.y != startY && !ducking) {
             this.y += jumpHeight;
         }
-        
-//        this.x += this.speedX;
-//        this.y += this.speedY;
+
+        //        this.x += this.speedX;
+        //        this.y += this.speedY;
     }
-    this.crashWith = function (otherobj){
+    this.crashWith = function (otherobj) {
         var myleft = this.x;
         var myright = this.x + (this.width);
         var mytop = this.y;
@@ -88,10 +86,10 @@ function component(width, height, color, x, y){
         var otherbottom = otherobj.y + (otherobj.height);
         var crash = true;
         if ((mybottom < othertop) ||
-               (mytop > otherbottom) ||
-               (myright < otherleft) ||
-               (myleft > otherright)) {
-           crash = false;
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            crash = false;
         }
         return crash;
     }
@@ -100,84 +98,85 @@ function component(width, height, color, x, y){
 
 
 function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    if ((myGameArea.frameNo / n) % 1 == 0) {
+        return true;
+    }
     return false;
 }
 
 
-function updateGameArea(){
+function updateGameArea() {
     var x, y;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
             myGameArea.stop();
             return;
-        } 
+        }
     }
     myGameArea.clear();
     myGameArea.frameNo += 1;
-      if (myGameArea.frameNo == 1 || everyinterval(200)) {
+    if (myGameArea.frameNo == 1 || everyinterval(200)) {
         x = myGameArea.canvas.width;
-//        minHeight = 20;
-//        maxHeight = 200;
-//        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-//        minGap = 80;
-//        maxGap = 200;
-//        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        //        minHeight = 20;
+        //        maxHeight = 200;
+        //        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        //        minGap = 80;
+        //        maxGap = 200;
+        //        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
         whichObstacle = Math.random();
         //Randomly determine which obstacle pops up
-        if(whichObstacle > 0.5){
+        if (whichObstacle > 0.5) {
             myObstacles.push(new component(30, 60, "green", x, 350));
-        }
-        else {
+        } else {
             myObstacles.push(new component(50, 30, "orange", x, 300));
         }
-//        myObstacles.push(new component(10, height, "green", x, 0));
-//        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+        //        myObstacles.push(new component(10, height, "green", x, 0));
+        //        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].x += -1;
         myObstacles[i].update();
     }
-    myGamePiece.newPos(); 
+    myGamePiece.newPos();
     myPlatform.update();
     myGamePiece.update();
 }
-    
+
 function moveup() {
-    myGamePiece.speedY -= 1; 
+    myGamePiece.speedY -= 1;
 }
 
 function movedown() {
     myGamePiece.speedY += 1;
 }
-    
-function stopMove(){
+
+function stopMove() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
 }
-    
-function jump(){
-    if(!jumping){
+
+function jump() {
+    if (!jumping) {
         jumping = true;
-        setTimeout(land, 1000);// calls land function after 500 ms
+        setTimeout(land, 1000); // calls land function after 500 ms
     }
 }
-    
-function land(){
+
+function land() {
     jumping = false;
 }
 
-function duck(){
-    if(!ducking){
+function duck() {
+    if (!ducking) {
         ducking = true;
         setTimeout(stand, 1200)
     }
 }
 
-function stand(){
+function stand() {
     ducking = false;
 }
-    
-function refreshGame(){
+
+function refreshGame() {
     location.reload();
 }
--- maingame/photo.js
+++ maingame/photo.js
@@ -1,70 +1,106 @@
-navigator.getUserMedia = (navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
-
-var video;
-var webcamStream;
-var canvas;
-
-function startWebcam() {
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia(
-
-            // constraints
-            {
-                video: true,
-                audio: false
-            },
-
-            // successCallback
-            function (localMediaStream) {
-                video = document.querySelector('video');
-                video.src = window.URL.createObjectURL(localMediaStream);
-                webcamStream = localMediaStream;
-            },
-
-            // errorCallback
-            function (err) {
-                alert("Camera not approved/other error reached. Please allow camera access to play if you selected block");
-                init();
-            }
-        );
-    } else {
-        alert("Sorry not compatible with your browser");
-    }
-}
-
-function init() {
-    startWebcam();
-    setTimeout(function () {
-        alert("YO");
-        getImages();
-    }, 5000);
-
-}
-
-function getImages() {
-
-    for (var i = 0; i < 1; i++) {
-        setTimeout(function () {
-            canvas = document.getElementById("myCanvas");
-            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-            var uri = canvas.toDataURL('image/png')
-            var res = encodeURI(uri);
-            console.log(res);
-        }, 10000);
-    }
-
-}
-
-function snapshot() {
-    // Draws current image from the video element into the canvas
-    //    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    //    callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
-    //    callback(ctx.toDataURL('image/png'));
-    //    console.log(ctx.toDataURL('image/png'));
-
-    alert("NEW PHOTO");
-}
-window.onload = init();
+//navigator.getUserMedia = (navigator.getUserMedia ||
+//    navigator.webkitGetUserMedia ||
+//    navigator.mozGetUserMedia ||
+//    navigator.msGetUserMedia);
+//
+//var video;
+//var webcamStream;
+//var canvas;
+//
+//function startWebcam() {
+//    if (navigator.getUserMedia) {
+//        navigator.getUserMedia(
+//
+//            // constraints
+//            {
+//                video: true,
+//                audio: false
+//            },
+//
+//            // successCallback
+//            function (localMediaStream) {
+//                video = document.querySelector('video');
+//                video.src = window.URL.createObjectURL(localMediaStream);
+//                webcamStream = localMediaStream;
+//            },
+//
+//            // errorCallback
+//            function (err) {
+//                alert("Camera not approved/other error reached. Please allow camera access to play if you selected block");
+//                init();
+//            }
+//        );
+//    } else {
+//        alert("Sorry not compatible with your browser");
+//    }
+//}
+//
+//function init() {
+//    startWebcam();
+//    setTimeout(function () {
+//        alert("YO");
+//        getImages();
+//    }, 5000);
+//
+//}
+//
+//function getImages() {
+//
+//    for (var i = 0; i < 1; i++) {
+//        setTimeout(function () {
+//            canvas = document.getElementById("myCanvas");
+//            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+//            var dataURL = canvas.toDataURL('image/png')
+//
+//        }, 10000);
+//    }
+//
+//}
+//makeblob = function (dataURL) {
+//    var BASE64_MARKER = ';base64,';
+//    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+//        var parts = dataURL.split(',');
+//        var contentType = parts[0].split(':')[1];
+//        var raw = decodeURIComponent(parts[1]);
+//        return new Blob([raw], {
+//            type: contentType
+//        });
+//    }
+//    var parts = dataURL.split(BASE64_MARKER);
+//    var contentType = parts[0].split(':')[1];
+//    var raw = window.atob(parts[1]);
+//    var rawLength = raw.length;
+//
+//    var uInt8Array = new Uint8Array(rawLength);
+//
+//    for (var i = 0; i < rawLength; ++i) {
+//        uInt8Array[i] = raw.charCodeAt(i);
+//    }
+//
+//    return new Blob([uInt8Array], {
+//        type: contentType
+//    });
+//} /**/
+//
+//var emotion_api_key = "abcdefghijklmnopqrstuvwzyz1234";
+//$.ajax({
+//        url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
+//        beforeSend: function (xhrObj) {
+//            // Request headers
+//            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+//            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", emotion_api_key);
+//        },
+//        type: "POST",
+//        // Request body
+//        data: makeblob(dataURL),
+//        processData: false,
+//        success: function (data) {
+//            var result = data;
+//            //code to show result will be here
+//        }
+//    })
+//    .fail(function (data) {
+//        alert("Code: " + data.responseJSON.error.code + " Message:" + data.responseJSON.error.message);
+//    });
+//
+//window.onload = init();
